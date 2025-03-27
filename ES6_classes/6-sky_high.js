83ef0d0c3f7051581036ed73e227f589298683a6
RayaneAll <9975@holbertonstudents.com>
2025-03-27 13:22:03 +0100

    Update code for 4-pricing.js and 6-sky_high.js fil

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,20 +1,16 @@
-import Building from './5-building.js';
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    super(sqft); // Appelle le constructeur de la classe parente Building
-    if (typeof floors !== 'number') {
-      throw new TypeError('Floors must be a number');
-    }
+    super(sqft);
+
     this._floors = floors;
   }
 
-  // Getter for floors
   get floors() {
     return this._floors;
   }
 
-  // Override evacuationWarningMessage
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors`;
   }
