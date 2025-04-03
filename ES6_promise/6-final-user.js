7f73ba389ff7e6467aabefc1df9218326a8df309
RayaneAll <9975@holbertonstudents.com>
2025-04-03 13:46:03 +0200

    Add code to the directory ES6_promise

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName).then((value) => ({
+    status: 'fulfilled',
+    value,
+  }));
+
+  const photoPromise = uploadPhoto(fileName).catch((error) => ({
+    status: 'rejected',
+    value: error.toString(),
+  }));
+
+  return Promise.allSettled([userPromise, photoPromise]);
+}
