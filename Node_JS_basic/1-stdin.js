35438d5a391098961765c3dbe24fdf38a198d4e9
RayaneAll <9975@holbertonstudents.com>
2025-05-20 16:41:22 +0200

    Update code for 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdin.setEncoding('utf-8');
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (input) => {
   const name = input.trim();
-  console.log('Your Name is: ${name}');
+  console.log(`Your name is: ${name}`);
 });
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
